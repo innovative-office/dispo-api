3702b4ef3a45f2998af0bed131df558e4037d59e
tzhbami7 <michael.balsiger@outlook.com>
2016-08-21 21:43:13 +0000

    bump deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, 'fraefel.kern-fusion.ch'
+set :domain, 'dispo-api.kern-fusion.ch'
 set :deploy_to, '/home/tzhbami7/apps/dispo-api'
 set :repository, 'https://github.com/innovative-office/dispo-api.git'
 set :branch, 'master'
@@ -20,7 +20,7 @@ set :branch, 'master'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
+set :shared_paths, ['config/application.yml', 'config/database.yml', 'config/secrets.yml', 'log']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
@@ -52,6 +52,7 @@ task :setup => :environment do
 
   queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
   queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/application.yml"]
   queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml' and 'secrets.yml'."]
 
   if repository
